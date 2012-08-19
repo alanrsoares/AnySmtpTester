82d1b00ad44c4e3e76de935095172c410c78b0f0
Alan Rodrigues Soares <alanrsoars@gmail.com>
2012-08-18 23:41:46 -0300

    Add AnySmtpTester.cs

---
+++ AnySmtpTester/AnySmtpTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnySmtpTester
+{
+    public class AnySmtpTester
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+        
+        #region Constructors
+        public AnySmtpTester()
+        {
+
+        }
+        #endregion
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+    }
+}
