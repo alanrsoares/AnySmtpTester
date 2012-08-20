1af1c49052ef51067cda6eced1ff1c42b2a4f0f8
Alan Rodrigues Soares <alanrsoars@gmail.com>
2012-08-19 21:21:40 -0300

    ok

---
+++ AnySmtpTester.Tests/AnySmtpTesterTest.cs
@@ -0,0 +1,162 @@
+﻿using AnySmtpTester;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Mail;
+
+namespace AnySmtpTester.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for AnySmtpTesterTest and is intended
+    ///to contain all AnySmtpTesterTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AnySmtpTesterTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for AnySmtpTester Constructor
+        ///</summary>
+        [TestMethod()]
+        public void AnySmtpTesterConstructorTest()
+        {
+            SmtpClient smtpClient = null; // TODO: Initialize to an appropriate value
+            AnySmtpTester target = new AnySmtpTester(smtpClient);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for AnySmtpTester Constructor
+        ///</summary>
+        [TestMethod()]
+        public void AnySmtpTesterConstructorTest1()
+        {
+            string host = string.Empty; // TODO: Initialize to an appropriate value
+            int port = 0; // TODO: Initialize to an appropriate value
+            bool enableSsl = false; // TODO: Initialize to an appropriate value
+            string user = string.Empty; // TODO: Initialize to an appropriate value
+            string password = string.Empty; // TODO: Initialize to an appropriate value
+            AnySmtpTester target = new AnySmtpTester(host, port, enableSsl, user, password);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for GetServerStatus
+        ///</summary>
+        [TestMethod()]
+        public void GetServerStatusTest()
+        {
+            SmtpClient smtpClient = null; // TODO: Initialize to an appropriate value
+            AnySmtpTester target = new AnySmtpTester(smtpClient); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.GetServerStatus();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for SendMessage
+        ///</summary>
+        [TestMethod()]
+        public void SendMessageTest()
+        {
+            SmtpClient smtpClient = null; // TODO: Initialize to an appropriate value
+            AnySmtpTester target = new AnySmtpTester(smtpClient); // TODO: Initialize to an appropriate value
+            string from = string.Empty; // TODO: Initialize to an appropriate value
+            string to = string.Empty; // TODO: Initialize to an appropriate value
+            string subject = string.Empty; // TODO: Initialize to an appropriate value
+            string body = string.Empty; // TODO: Initialize to an appropriate value
+            object expected = null; // TODO: Initialize to an appropriate value
+            object actual;
+            actual = target.SendMessage(from, to, subject, body);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for SendTestMessage
+        ///</summary>
+        [TestMethod()]
+        public void SendTestMessageTest()
+        {
+            SmtpClient smtpClient = null; // TODO: Initialize to an appropriate value
+            AnySmtpTester target = new AnySmtpTester(smtpClient); // TODO: Initialize to an appropriate value
+            string from = string.Empty; // TODO: Initialize to an appropriate value
+            string to = string.Empty; // TODO: Initialize to an appropriate value
+            object expected = null; // TODO: Initialize to an appropriate value
+            object actual;
+            actual = target.SendTestMessage(from, to);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Client
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("AnySmtpTester.dll")]
+        public void ClientTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            AnySmtpTester_Accessor target = new AnySmtpTester_Accessor(param0); // TODO: Initialize to an appropriate value
+            SmtpClient expected = null; // TODO: Initialize to an appropriate value
+            SmtpClient actual;
+            target.Client = expected;
+            actual = target.Client;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
--- AnySmtpTester/AnySmtpTester.cs
+++ AnySmtpTester/AnySmtpTester.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
 using System.Text;
 
 namespace AnySmtpTester
@@ -13,22 +14,133 @@ namespace AnySmtpTester
 
         #region Properties
 
+        public SmtpClient Client { get; private set; }
+
         #endregion
 
         #region Public Methods
-        
-        #region Constructors
-        public AnySmtpTester()
-        {
 
+        #region Constructors
+        public AnySmtpTester(SmtpClient smtpClient)
+        {
+            Client = smtpClient;
         }
+
+        public AnySmtpTester(string host, int port, bool enableSsl, string user, string password)
+        {
+            Client = new SmtpClient
+                         {
+                             Host = host,
+                             Port = port,
+                             EnableSsl = enableSsl,
+                             UseDefaultCredentials = false,
+                             Credentials = new NetworkCredential(user, password)
+                         };
+        }
+
         #endregion
+
+        public string GetServerStatus()
+        {
+            return "";
+        }
+
+        public object SendMessage(string from, string to, string subject, string body)
+        {
+            if (string.IsNullOrEmpty(from))
+            {
+                throw new ArgumentNullException("from", "Required parameter (from) is missing.");
+            }
+            if (string.IsNullOrEmpty(to))
+            {
+                throw new ArgumentNullException("to", "Required parameter (to) is missing.");
+            }
+            if (string.IsNullOrEmpty(body))
+            {
+                throw new ArgumentNullException("body", "Required parameter (body) is missing.");
+            }
+
+            var message = new MailMessage
+            {
+                From = new MailAddress(from),
+                Subject = subject ?? "",
+                Body = body,
+            };
+
+            message.To.Add(new MailAddress(to));
+
+            try
+            {
+                Client.Send(message);
+            }
+            catch (Exception e)
+            {
+                return new { Success = false, e.Message };
+            }
+
+            return new { Success = true };
+        }
+
+        public object SendTestMessage(string from, string to)
+        {
+            return SendMessage(from, to, "[AnySmtpTester] - SendTestMessage", "Test Message from AnySmtpTester.");
+        }
 
         #endregion
 
         #region Private Methods
+        
 
         #endregion
 
+    }
+
+    public static class SmtpHelper
+    {
+        /// <summary>
+        /// test the smtp connection by sending a HELO command
+        /// </summary>
+        /// <param name="smtpServerAddress"></param>
+        /// <param name="port"></param>
+        public static bool TestConnection(string smtpServerAddress, int port)
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(smtpServerAddress);
+            var endPoint = new IPEndPoint(hostEntry.AddressList[0], port);
+            using (var tcpSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                //try to connect and test the rsponse for code 220 = success
+                tcpSocket.Connect(endPoint);
+                if (!CheckResponse(tcpSocket, 220))
+                {
+                    return false;
+                }
+
+                // send HELO and test the response for code 250 = proper response
+                SendData(tcpSocket, string.Format("HELO {0}\r\n", Dns.GetHostName()));
+
+                return CheckResponse(tcpSocket, 250);
+
+                // if we got here it's that we can connect to the smtp server
+            }
+        }
+
+        private static void SendData(Socket socket, string data)
+        {
+            var dataArray = Encoding.ASCII.GetBytes(data);
+            socket.Send(dataArray, 0, dataArray.Length, SocketFlags.None);
+        }
+
+        private static bool CheckResponse(Socket socket, int expectedCode)
+        {
+            while (socket.Available == 0)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+            var responseArray = new byte[1024];
+            socket.Receive(responseArray, 0, socket.Available, SocketFlags.None);
+            var responseData = Encoding.ASCII.GetString(responseArray);
+            var responseCode = Convert.ToInt32(responseData.Substring(0, 3));
+            return responseCode == expectedCode;
+        }
     }
 }
